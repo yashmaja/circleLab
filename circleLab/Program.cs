e8b876844ab0411e8777966d7d47053413125be2
Yashashree Majalikar <yashmaja@gmail.com>
2021-10-12 10:10:22 -0400

    added validator

--- circleLab/Program.cs
+++ circleLab/Program.cs
@@ -9,29 +9,13 @@ namespace circleLab
             Console.WriteLine("Welcome to the Circle Tester");
 
             bool repeat = true;
-            double radius = 0;
             int numOfCircles = 0;
 
             while (repeat)
             {
                 Console.Write("Enter radius: ");
 
-                while (true)
-                {
-                    try
-                    {
-                        radius = double.Parse(Console.ReadLine());
-                        if (radius < 0)
-                        {
-                            throw new Exception("Please enter a positive number.");
-                        }
-                        break;
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Invalid input. Please enter numbers only. ");
-                    }
-                }
+                double radius = Validator.getRadius();
 
                 Circle newCircle = new Circle(radius);
                 numOfCircles++;
@@ -39,33 +23,10 @@ namespace circleLab
                 Console.WriteLine($"Circumference: {newCircle.CalculateFormattedCircumference()}.");
                 Console.WriteLine($"Area: {newCircle.CalculateFormattedArea()}");
 
-                repeat = Repeat();
+                repeat = Validator.Repeat();
             }
 
             Console.WriteLine($"You created {numOfCircles} circles.");
-        }
-
-        public static bool Repeat()
-        {
-            while (true)
-            {
-                Console.Write("\nAnother circle? (y/n): ");
-                string answer = Console.ReadLine().ToLower();
-
-                if (answer == "n")
-                {
-                    Console.WriteLine("\nGoodbye!");
-                    return false;
-                }
-                else if (answer == "y")
-                {
-                    return true;
-                }
-                else
-                {
-                    Console.WriteLine("\nInvalid input. Try again.\n");
-                }
-            }
         }
     }
 }
--- circleLab/Validator.cs
+++ circleLab/Validator.cs
@@ -6,17 +6,51 @@ namespace circleLab
 {
     class Validator
     {
-        //properties
-        private double Input;
-
-        //constructors
-        /*public double IsDouble
+        public static double getRadius()
         {
+            double radius = 0;
+            while (true)
+            {
+                try
+                {
+                    radius = double.Parse(Console.ReadLine());
+                    if (radius < 0)
+                    {
+                        throw new Exception("Please enter a positive number.");
+                    }
+                    break;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Invalid input. Please enter numbers only. ");
+                }
+            }
 
+            return radius;
         }
-        */
 
-        //methods
+        public static bool Repeat()
+        {
+            while (true)
+            {
+                Console.Write("\nAnother circle? (y/n): ");
+                string answer = Console.ReadLine().ToLower();
+
+                if (answer == "n")
+                {
+                    Console.WriteLine("\nGoodbye!");
+                    return false;
+                }
+                else if (answer == "y")
+                {
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid input. Try again.\n");
+                }
+            }
+        }
 
     }
 }
